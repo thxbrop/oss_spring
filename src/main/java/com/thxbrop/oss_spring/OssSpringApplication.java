9351b3b52b7db23eed8b9f118b49b302fedb8e42
thxbrop <1365427005@qq.com>
2022-05-30 16:02:14 +0800

    Initial commit

---
+++ src/main/java/com/thxbrop/oss_spring/OssSpringApplication.java
@@ -0,0 +1,13 @@
+package com.thxbrop.oss_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OssSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/thxbrop/oss_spring/entity/Cargo.java
@@ -0,0 +1,2 @@
+package com.thxbrop.oss_spring.entity;public class Cargo {
+}
